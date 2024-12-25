b7ae1d8e8a855ff635d97c5ec1cdd25890ff1673
VDFarsH <81068681+VDFarsH@users.noreply.github.com>
2024-12-25 12:00:43 +0900

    Add files via upload

---
+++ lesson 76 js/main.js
@@ -0,0 +1,12 @@
+const firstName = 'Вадим'
+let age = 20
+
+var lastName = 'Кондаков'
+
+let statusEd = 'Cтудент онлайн школы Aroken.ru'
+
+const myBirthday = '31.10.2004'
+
+console.log(firstName, lastName, age, 'y.o.')
+console.log(statusEd)
+console.log('День рождения:', myBirthday)
